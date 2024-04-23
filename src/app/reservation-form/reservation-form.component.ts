1bde9d6eb8d6013a2e9dc1da278481a14744969b
sidchopp <sidchopp@gmail.com>
2024-04-23 14:08:01 -0600

    reservation-form started

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reservation-form',
   templateUrl: './reservation-form.component.html',
-  styleUrls: ['./reservation-form.component.css']
+  styleUrls: ['./reservation-form.component.css'],
 })
 export class ReservationFormComponent {
-
+  reservationForm: FormGroup = new FormGroup({});
+  onSubmit() {}
 }

67a7ccc8de66dc6cb0b31fbfcd5908506f45ef5f
sidchopp <sidchopp@gmail.com>
2024-04-24 10:37:37 -0600

    Form validation created

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -6,7 +6,22 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './reservation-form.component.html',
   styleUrls: ['./reservation-form.component.css'],
 })
-export class ReservationFormComponent {
+export class ReservationFormComponent implements OnInit {
   reservationForm: FormGroup = new FormGroup({});
+
+  // Dependency injection - form builder will be invoked/injected On init
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.reservationForm = this.formBuilder.group({
+      // the keys on left below should match with the values of formControlName in respective html
+      checkInDate: ['', Validators.required],
+      checkOutDate: ['', Validators.required],
+      guestName: ['', Validators.required],
+      guestEmail: ['', [Validators.required, Validators.email]],
+      roomNumber: ['', Validators.required],
+    });
+  }
+
   onSubmit() {}
 }

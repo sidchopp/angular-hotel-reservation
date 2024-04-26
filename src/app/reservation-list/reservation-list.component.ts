ba13338bd213715da809910bbe09666b0beb09dd
sidchopp <sidchopp@gmail.com>
2024-04-26 15:09:11 -0600

    Reservations List component worked on

--- src/app/reservation-list/reservation-list.component.ts
+++ src/app/reservation-list/reservation-list.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReservationModule } from '../reservation/reservation.module';
+import { Reservation } from '../models/reservation';
+import { ReservationService } from '../reservation/reservation.service';
 
 @Component({
   selector: 'app-reservation-list',
   templateUrl: './reservation-list.component.html',
-  styleUrls: ['./reservation-list.component.css']
+  styleUrls: ['./reservation-list.component.css'],
 })
-export class ReservationListComponent {
+export class ReservationListComponent implements OnInit {
+  // Data binding
+  reservations: Reservation[] = [];
 
+  // Dependency injection
+  constructor(private reservationService: ReservationService) {}
+
+  // On init , the reservations property grabs all the reservations from the service
+  ngOnInit(): void {
+    this.reservations = this.reservationService.getReservations();
+  }
 }

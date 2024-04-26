f60f5fe6cb6ed4c5768335ff87602ab6f9c7b726
sidchopp <sidchopp@gmail.com>
2024-04-26 14:56:38 -0600

    created localStorage in reservation service

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -7,6 +7,14 @@ import { Reservation } from '../models/reservation';
 export class ReservationService {
   private reservations: Reservation[] = [];
 
+  // load constructor before invoking onInit lifecycle hook
+  constructor() {
+    let savedReservations = localStorage.getItem('reservations');
+
+    // if key ( reservations) is there, than convert JSON string into a JavaScript object or gives an empty array
+    this.reservations = savedReservations ? JSON.parse(savedReservations) : [];
+  }
+
   //CRUD
   getReservations(): Reservation[] {
     return this.reservations;
@@ -18,13 +26,13 @@ export class ReservationService {
 
   addReservation(reservation: Reservation): void {
     this.reservations.push(reservation);
-    //testing
-    console.log(this.reservations);
+    localStorage.setItem('reservations', JSON.stringify(this.reservations));
   }
 
   deleteReservation(id: string): void {
     let index = this.reservations.findIndex((res) => res.id === id);
     this.reservations.splice(index, 1);
+    localStorage.setItem('reservations', JSON.stringify(this.reservations));
   }
 
   updateReservation(updatedReservation: Reservation): void {
@@ -32,5 +40,6 @@ export class ReservationService {
       (res) => res.id === updatedReservation.id
     );
     this.reservations[index] = updatedReservation;
+    localStorage.setItem('reservations', JSON.stringify(this.reservations));
   }
 }
